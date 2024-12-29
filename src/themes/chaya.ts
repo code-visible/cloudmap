9010c4dc0854af25f05a6e0e037ae184e16de6fb
ChallenAi <fengyiai@gmail.com>
2024-12-29 22:47:47 +0000

    feat: add new theme: mayk

---
+++ src/themes/chaya.ts
@@ -0,0 +1,13 @@
+import { Palette } from "./theme";
+
+const chaya: Palette = {
+  highlight: "#393",
+  focus: "#171",
+  muted: "#999",
+  arrow: "#ccc",
+  card: "#666",
+  seperator: "#000",
+  cardShadow: "rgba(150, 150, 150, .2)",
+};
+
+export default chaya;
