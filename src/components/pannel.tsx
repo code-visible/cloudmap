dc22bd0284a65a8c44005bb2efe7321a9653c813
ChallenAi <fengyiai@gmail.com>
2024-12-31 16:43:54 +0000

    feat: render directories and files in pannel

--- src/components/pannel.tsx
+++ src/components/pannel.tsx
@@ -1,4 +1,4 @@
-import { Pkg } from '../resource/node';
+import { Dir, File, Pkg } from '../resource/node';
 import { GraphType, StateCall, StateFile, StatePkg, StateTheme } from '../state';
 import data from '../data';
 
@@ -15,46 +15,96 @@ export interface PannelProps {
   setPkg: (s: StatePkg) => void;
 };
 
-const pkgs: Pkg[] = [];
-for (const el of data.pkgs.values()) {
-  pkgs.push(el);
-}
-pkgs.sort((a: Pkg, b: Pkg) => a.path.localeCompare(b.path));
-
 function Pannel({ pkg, theme, setPkg, setGraphType }: PannelProps) {
 
-  const handleSelectPkg = (id: string) => {
+  const activePkg = data.pkgs.get(pkg.active);
+
+  const getDirColor = (dir: Dir) => {
+    if (!dir.pkgPtr) return theme.palette.muted;
+    const pkgID = dir.pkgPtr.ref.id;
+    if (pkgID === pkg.entrance) return theme.palette.focus;
+    if (pkgID === pkg.active) return theme.palette.highlight;
+    if (pkg.set.has(pkgID) && activePkg && (activePkg.imports.has(dir.pkgPtr) || activePkg.exports.has(dir.pkgPtr))) return theme.palette.highlight;
+    return theme.palette.muted;
+  };
+
+  const renderDirectory = (dir: Dir): any => {
+    return (
+      <li
+        key={dir.path}
+        className={styles.dir}
+      >
+        <div
+          className={styles.dirname}
+          onClick={() => handleSelectDir(dir)}
+          style={{
+            color: getDirColor(dir),
+          }}
+        >{dir.name}</div>
+        <ul className={styles.dirlist}>
+          {renderDirectories(dir.children)}
+          {renderFiles(dir.files)}
+        </ul>
+      </li>
+    );
+  };
+
+  const renderFile = (file: File): any => {
+    return (
+      <li
+        key={file.ref.id}
+        className={styles.file}
+      >
+        <div
+          className={styles.filename}
+        >{file.ref.name}</div>
+      </li>
+    );
+  };
+
+  const renderFiles = (files: Set<File>): any => {
+    const result = [];
+    for (const file of files) {
+      result.push(renderFile(file));
+    }
+    return result;
+  };
+
+  const renderDirectories = (dirs: Set<Dir>): any => {
+    const result = [];
+    for (const dir of dirs) {
+      result.push(renderDirectory(dir));
+    }
+    return result;
+  };
+
+  const handleSelectDir = (dir: Dir) => {
+    if (!dir.pkgPtr) return;
+    const id = dir.pkgPtr.ref.id;
     setGraphType(GraphType.PKG);
     const pkgSet = data.getPkgsByRoot(id, 12);
     setPkg({ entrance: id, active: "", set: pkgSet });
   };
 
-  const activePkg = data.pkgs.get(pkg.active);
 
-  const getPkgColor = (p: Pkg) => {
-    const pkgID = p.ref.id;
-    if (pkgID === pkg.entrance) return theme.palette.focus;
-    if (pkgID === pkg.active) return theme.palette.highlight;
-    if (pkg.set.has(pkgID) && activePkg && (activePkg.imports.has(p) || activePkg.exports.has(p))) return theme.palette.highlight;
-    return theme.palette.muted;
-  };
 
   return (
     <div className={styles.pannel}>
       <ul>
         {
-          pkgs.map((p: Pkg) => (
-            <li
-              key={p.ref.id}
-              className={styles.pkg}
-              style={{
-                color: getPkgColor(p),
-              }}
-              onClick={() => handleSelectPkg(p.ref.id)}
-            >
-              {p.path}
-            </li>
-          ))
+          // pkgs.map((p: Pkg) => (
+          //   <li
+          //     key={p.ref.id}
+          //     className={styles.pkg}
+          //     style={{
+          //       color: getPkgColor(p),
+          //     }}
+          //     onClick={() => handleSelectPkg(p.ref.id)}
+          //   >
+          //     {p.path}
+          //   </li>
+          // ))
+          renderDirectory(data.root)
         }
       </ul>
     </div>
