290bf52a739ce55a1abeb9dcdcd4a1027b1903df
ChallenAi <fengyiai@gmail.com>
2024-12-31 10:21:33 +0000

    chore: remove unused parameters

--- src/graph/ui/pkg/cardfield.ts
+++ src/graph/ui/pkg/cardfield.ts
@@ -34,12 +34,12 @@ export const buildCardField = (x: number, y: number, key: string, val: string): 
     contain(x, y) {
       return x > 0 && x < 192 && y > -10 && y < 0;
     },
-    onMouseenter(render, x, y, mx, my) {
+    onMouseenter(render) {
       this.texts![0].opts!.fill = "#993";
       render();
       return true;
     },
-    onMouseleave(render, x, y, mx, my) {
+    onMouseleave(render) {
       this.texts![0].opts!.fill = "#666";
       render();
       return true;
