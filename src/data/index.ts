aed684d71e02b2483939a3210ff4cd1b908c5cc9
ChallenAi <fengyiai@gmail.com>
2025-01-14 16:17:32 +0000

    refactor: remove legacy data loader

--- src/data/index.ts
+++
@@ -1,8 +0,0 @@
-import source from '../testdata/prom.json';
-import { SourceMap } from '../resource/resource';
-import type { Source } from '../protocol/map';
-
-const data = new SourceMap();
-data.parseSource(source as Source);
-
-export default data;
